3ea0d60a130ff8d1a75ee8feef02bdd14e508741
ziqingchuan <221250108@smail.nju.edu.cn>
2025-08-01 10:17:03 +0800

    2025-08-01

--- src/consts/markdownContent.ts
+++ src/consts/markdownContent.ts
@@ -1,5 +1,5 @@
-export const initialMarkdownContent = `# 欢迎来到素笔 Mark !
-
+export const initialMarkdownContent = `
+# 欢迎来到素笔 Mark !
 这是一个基于Vue3的简易Markdown编辑器，提供如下功能：
 > - 实时预览
 > - 明暗模式切换
@@ -12,6 +12,8 @@ export const initialMarkdownContent = `# 欢迎来到素笔 Mark !
 > - 代码高亮
 > - 快捷检索emoji
 > - 快捷工具栏
+> - html代码渲染模式
+> - json结构可视化定义
 > - 后续持续更新...
 
 [欢迎来我的博客看看：try-catch.life](https://try-catch.life/)
