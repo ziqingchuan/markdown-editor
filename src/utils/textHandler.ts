51cadbe61214bcbfa0a473d560ebae7c7209e5ea
ziqingchuan <221250108@smail.nju.edu.cn>
2025-09-04 15:29:31 +0800

    优化工具栏体验；优化行内公式显示问题

--- src/utils/textHandler.ts
+++ src/utils/textHandler.ts
@@ -3,29 +3,29 @@ import type {ContentType} from "../types/contentType.ts";
 export const textHandler = (type: ContentType, selectedText: string) => {
     switch (type) {
         case 'bold':
-            selectedText = `**${selectedText}**`;
+            selectedText = `**${selectedText === '' ? '加粗内容' : selectedText}**`;
             break;
         case 'italic':
-            selectedText = `*${selectedText}*`;
+            selectedText = `*${selectedText === '' ? '斜体内容' : selectedText}*`;
             break;
         case 'underline':
-            selectedText = `<u>${selectedText}</u>`; // Markdown原生不支持下划线，用HTML标签
+            selectedText = `<u>${selectedText === '' ? '下划线内容' : selectedText}</u>`; // Markdown原生不支持下划线，用HTML标签
             break;
         case 'link':
             const linkText = selectedText || '链接文本';
             selectedText = `[${linkText}](https://example.com)`; // 默认链接地址，可改为prompt输入
             break;
         case 'code':
-            selectedText = `\`${selectedText}\``; // 行内代码
+            selectedText = `\`${selectedText === '' ? 'code' : selectedText}\``; // 行内代码
             break;
         case 'quote':
-            selectedText = `> ${selectedText}`;
+            selectedText = `> ${selectedText === '' ? '引用内容' : selectedText}`;
             break;
         case 'list':
-            selectedText = `- ${selectedText}`
+            selectedText = `- ${selectedText === '' ? '列表内容' : selectedText}`
             break;
         case 'math':
-            selectedText = ` $${selectedText}$`;
+            selectedText = ` $${selectedText === '' ? 'a ^ 2 + b ^ 2 = c ^ 2' : selectedText}$ `;
             break;
     }
     return selectedText;
